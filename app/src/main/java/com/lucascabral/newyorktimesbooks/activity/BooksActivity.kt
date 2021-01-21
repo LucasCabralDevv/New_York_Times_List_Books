5cb5d83b44b38a933d27cec956f55d51f5205345
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-01-21 18:32:38 -0300

    Projeto configurado.

--- app/src/main/java/com/lucascabral/newyorktimesbooks/activity/BooksActivity.kt
+++ app/src/main/java/com/lucascabral/newyorktimesbooks/activity/BooksActivity.kt
@@ -1,12 +1,21 @@
 package com.lucascabral.newyorktimesbooks.activity
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lucascabral.newyorktimesbooks.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        toolbarMain.title = getString(R.string.toolbarMain_title)
+        setSupportActionBar(toolbarMain)
     }
 }
