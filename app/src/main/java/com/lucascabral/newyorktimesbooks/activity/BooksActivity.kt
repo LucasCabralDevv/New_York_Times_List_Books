e1920d499ac1d666784e41606de9b05fc0f24071
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-01-21 16:36:32 -0300

    Projeto organizado.

---
+++ app/src/main/java/com/lucascabral/newyorktimesbooks/activity/BooksActivity.kt
@@ -0,0 +1,12 @@
+package com.lucascabral.newyorktimesbooks.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lucascabral.newyorktimesbooks.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
