86a6a41e24268600899ef47689acd8e1204f2375
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-01-22 10:11:03 -0300

    RecyclerView finalizada.

---
+++ app/src/main/java/com/lucascabral/newyorktimesbooks/adapter/BooksAdapter.kt
@@ -0,0 +1,36 @@
+package com.lucascabral.newyorktimesbooks.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lucascabral.newyorktimesbooks.R
+import com.lucascabral.newyorktimesbooks.data.model.Book
+import kotlinx.android.synthetic.main.item_book.view.*
+
+class BooksAdapter(
+        private val books: List<Book>
+) : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+        return BooksViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        holder.bindView(books[position])
+    }
+
+    override fun getItemCount() = books.count()
+
+    inner class BooksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val title = itemView.bookTitleTextView
+        private val author = itemView.bookAuthorTextView
+
+        fun bindView(book: Book) {
+            title.text = book.title
+            author.text = book.author
+        }
+    }
+}
---
+++ app/src/main/java/com/lucascabral/newyorktimesbooks/data/model/Book.kt
@@ -0,0 +1,6 @@
+package com.lucascabral.newyorktimesbooks.data.model
+
+data class Book (
+        val title: String?,
+        val author: String
+)
