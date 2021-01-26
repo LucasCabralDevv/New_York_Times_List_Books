6526270ef0ddc50d3976a257ab54666801499c3d
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-01-26 10:34:53 -0300

    Toolbar configurada.

--- app/src/main/java/com/lucascabral/newyorktimesbooks/view/BookDetailsActivity.kt
+++ app/src/main/java/com/lucascabral/newyorktimesbooks/view/BookDetailsActivity.kt
@@ -14,15 +14,15 @@ class BookDetailsActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_details)
 
-        setupToolbar(toolbarMain, R.string.toolbarMain_details_title)
+        setupToolbar(toolbarMain, R.string.toolbarMain_details_title, true)
 
         bookDetailsTitle.text = intent.getStringExtra(EXTRA_TITLE)
         bookDetailsDescription.text = intent.getStringExtra(EXTRA_DESCRIPTION)
     }
 
     companion object {
-        private const val EXTRA_TITLE = "extraTitle"
-        private const val EXTRA_DESCRIPTION = "extraDescription"
+        private const val EXTRA_TITLE = "EXTRA_TITLE"
+        private const val EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION"
 
         fun getStartIntent(context: Context, title: String, description: String): Intent {
             return Intent(context, BookDetailsActivity::class.java).apply {
--- app/src/main/java/com/lucascabral/newyorktimesbooks/view/base/BaseActivity.kt
+++ app/src/main/java/com/lucascabral/newyorktimesbooks/view/base/BaseActivity.kt
@@ -3,10 +3,13 @@ package com.lucascabral.newyorktimesbooks.view.base
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
-open class BaseActivity: AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
 
-    protected fun setupToolbar(toolbar: Toolbar, titleIdRes: Int) {
+    protected fun setupToolbar(toolbar: Toolbar, titleIdRes: Int, showBackButton: Boolean = false) {
         toolbar.title = getString(titleIdRes)
         setSupportActionBar(toolbar)
+        if (showBackButton) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
