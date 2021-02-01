753694566fb2a20d6b7b10030922241cc8ae288b
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-02-01 16:38:21 -0300

    Criando teste unitário para o projeto.

---
+++ app/src/test/java/com/lucascabral/newyorktimesbooks/viewmodeltest/BooksViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.lucascabral.newyorktimesbooks.viewmodeltest
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
+class BooksViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+}
